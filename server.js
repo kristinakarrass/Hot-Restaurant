0d0e40b4b1580f3386e9be16474168bca98a291b
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-14 23:05:07 -0400

    working

---
+++ server.js
@@ -0,0 +1,70 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var fs = require("fs");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+//arrays to hold reservations and waitlist
+var reservations = [];
+
+var waitlist = [];
+
+//routes for HTML pages
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function(req, res) {
+	res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+	res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+//routes for api calls
+app.get("/api/tables", function(req, res) {
+	return res.json(reservations);
+});
+
+app.get("/api/waitlist", function(req, res) {
+	return res.json(waitlist);
+});
+
+//adding new reservation to array and text file
+app.post("/api/tables", function(req, res) {
+	var newreservation = req.body;
+
+	// newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase;
+
+	console.log(newreservation);
+
+	// fs.appendFile("data.txt", JSON.toString(newreservation), function(err) {
+	// 	if (err) throw err;
+	// });
+
+	// res.json(newreservation);
+	console.log(reservations.length);
+	if (reservations.length < 5) {
+		reservations.push(newreservation);
+		res.json(true)
+		} else {
+		waitlist.push(newreservation);
+		res.json(false)
+		}
+		console.log(reservations);
+});
+
+//==========================================================
+//listener
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT " + PORT);
+});
